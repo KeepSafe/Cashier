85d9269db614b74fb3cc4f65a3715d4cefab038c
Hilal Alsibai <xiphirx@gmail.com>
2016-02-12 12:01:42 -0800

    Code Style: Remove final

--- lib/src/main/java/com/getkeepsafe/cashier/ConsumeListener.java
+++ lib/src/main/java/com/getkeepsafe/cashier/ConsumeListener.java
@@ -4,5 +4,5 @@ import android.support.annotation.NonNull;
 
 public interface ConsumeListener {
     void success(@NonNull Purchase purchase);
-    void failure(@NonNull Purchase purchase, @NonNull final Vendor.Error error);
+    void failure(@NonNull Purchase purchase, @NonNull Vendor.Error error);
 }
